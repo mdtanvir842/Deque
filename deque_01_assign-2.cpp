3587ee13530fbd5d3aed4bddda3116607a5ab9ee
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-30 10:50:39 +0600

    Create deque_01_assign-2.cpp

---
+++ deque_01_assign-2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    deque<int>dq;
+    for(int i=0;i<5;i++){
+       cin>>dq[i];
+    }
+    for(int i=0;i<5;i++){
+       cout<<dq[i]<<endl;
+    }
+
+}
