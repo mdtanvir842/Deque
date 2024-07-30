d5d386b1dd723e470d13696523dca8ed82f98201
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-30 10:45:10 +0600

    Create deque_01_assign.cpp

---
+++ deque_01_assign.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    deque<int>dq;
+    dq.push_front(5);
+    dq.push_front(6);
+    dq.push_front(7);
+    dq.push_back(8);
+    dq.push_back(9);
+    dq.push_back(10);
+    cout<<dq.front()<<" "<<dq.back()<<endl;
+    dq.pop_front();
+    cout<<dq.front()<<" "<<dq.back()<<endl;
+    dq.pop_back();
+    cout<<dq.front()<<" "<<dq.back()<<endl;
+}
