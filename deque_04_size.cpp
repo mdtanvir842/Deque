c3f752e8b05917da4298b4dc0766f3e1a0f6fc6f
Md Tanvir Alam <mdtanviralam842@gmail.com>
2024-07-30 10:58:10 +0600

    Create deque_04_size.cpp

---
+++ deque_04_size.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    deque<int>dq;
+    dq.push_front(5);
+    dq.push_front(6);
+    dq.push_front(7);
+    dq.push_back(8);
+    dq.push_back(9);
+    dq.push_back(10);
+    cout<<dq.size()<<endl;
+}
